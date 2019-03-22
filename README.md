9aca726a8ca7e2deab054c36f96575d33592abee
Enrico Hoffmann <dasrick@gmail.com>
2019-03-22 16:08:34 +0100

    add mor info and some steps of testing

--- README.md
+++ README.md
@@ -14,4 +14,6 @@
 # some useful links
 
 * [MS Teams - adaptive cards](https://docs.microsoft.com/de-de/outlook/actionable-messages/adaptive-card)
+* [MS Teams - send via connectors](https://docs.microsoft.com/de-de/outlook/actionable-messages/send-via-connectors)
+
 ***
--- send_test.go
+++ send_test.go
@@ -17,8 +17,8 @@ func TestSend(t *testing.T) {
 			reqUrl: "https://outlook.office.com/webhook/a42444f3-d59e-4caf-b979-6df7919460b7@04ebf399-0553-42f8-9e20-599e669641dd/IncomingWebhook/6303c4b585b34d5986ad2e0fe0ddf3e2/2051545f-8c1d-4a1c-a2cd-1b90a24a0b99",
 			reqMsg: message{
 				Title:      "title",
-				Text:       "text",
-				ThemeColor: "#ff00cc",
+				Text:       "text **bold** and *italic* annd ***both*** ... some ~~strike through - doent work~~ but what happens if <br> * this<br> * is<br> * a<br> * list<br>",
+				ThemeColor: "ff00cc",
 			},
 			error: nil,
 		},
